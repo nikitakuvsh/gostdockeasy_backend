566aa11abb0fe6759e0f8413f9243bc4eeee2046
nikitakuvsh <world_fre@mail.ru>
2025-05-14 21:39:59 +0300

    mvp prod

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-DATABASE_URL = "postgresql+asyncpg://gostdockeasy_user:tri4Wq1P0hKuUTAOi0jMx41nHoyGYftL@dpg-d0ido1mmcj7s73dif520-a:5432/gostdockeasy"
+DATABASE_URL = "postgresql+asyncpg://gostdockeasy_user:tri4Wq1P0hKuUTAOi0jMx41nHoyGYftL@dpg-d0ido1mmcj7s73dif520-a.oregon-postgres.render.com:5432/gostdockeasy"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
